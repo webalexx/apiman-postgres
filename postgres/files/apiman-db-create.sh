2d1d6d62b61517f9554996b0b1e8e2194702cdb6
webalexx <webalexx@gmail.com>
2017-04-10 17:11:57 +0200

    #6

--- postgres/files/apiman-db-create.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "******CREATING APIMAN DATABASE******"
-#su -c "psql -d apiman -f /docker-entrypoint-initdb.d/apiman_postgresql.sql"  -s /bin/sh postgres
-gosu  postgres postgres --single -j apiman < /docker-entrypoint-initdb.d/apiman_postgresql.sql
-echo ""
-echo "******APIMAN DATABASE CREATED******"
